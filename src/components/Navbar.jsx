ea694c9a02ed64e81fc83f99c020ac331ae41c06
hitanshu0729 <hitanshu0729@gmail.com>
2024-07-06 12:18:34 +0530

    s

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,27 +2,33 @@ import React, { useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useFirebase } from "../context/Firebase";
 
 const MyNavbar = () => {
   const firebase = useFirebase();
+  const getLinkClassName = (path) => {
+    return location.pathname === path
+      ? "text-[orange] text-2xl"
+      : "text-white text-2xl";
+  };
+  const location = useLocation();
   useEffect(() => {}, [firebase.user]);
   const logout = () => {
     firebase.logout();
   };
   return (
     <Navbar className="bg-black h-[50px] flex items-center justify-around sticky top-0">
-      <Link to="/" className="text-[orange] text-2xl">
+      <Link to="/" className={getLinkClassName("/")}>
         Home
       </Link>
       {firebase.user && (
-        <Link to="/addpeople" className="text-white text-2xl">
+        <Link to="/addpeople" className={getLinkClassName("/addpeople")}>
           Add people
         </Link>
       )}
       {!firebase.user && (
-        <Link to="/login" className="text-white text-2xl">
+        <Link to="/login" className={getLinkClassName("/login")}>
           Login
         </Link>
       )}
